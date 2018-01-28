acfb95b80674349d094a7af76a77d2036cd150a6
Ma\admin <edwardma1013@gmail.com>
2018-01-28 12:21:38 +0800

    no message

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+    res.render('main');
+});
+
+app.listen(1234, function(){
+    console.log("Success.");
+})
